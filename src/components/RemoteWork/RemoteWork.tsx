18a60651d89434ae7c5248bf352d24063be11418
Dmitry <dimakurgan123789@gmail.com>
2023-04-08 23:05:06 +0300

    last refactoring

--- src/components/RemoteWork/RemoteWork.tsx
+++ src/components/RemoteWork/RemoteWork.tsx
@@ -5,6 +5,7 @@ import {Button} from '../../common/Button/Button';
 import {Title} from '../../common/Title/Title';
 import {strings} from "../../common/Utills/initialization.ts";
 
+
 type RemoteWorkPropsType = {}
 export const RemoteWork: React.FC<RemoteWorkPropsType> = ({}) => {
     return (
